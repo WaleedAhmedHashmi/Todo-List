f1a03a09f995ec8bab84498c88e0c91bf11d693f
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-05-08 21:59:32 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+let list = []
+let condition = true;
+
+while (condition) {
+    
+
+let todo_list = await inquirer.prompt([
+   
+   
+    {
+    name: "firstQ",
+    type: "input",
+    message: "What do you like to add in list ",
+    
+   },
+   {
+     name: "secondQ",
+     type: "confirm",
+     message: "Would you like to add more list",
+     default: "true"
+   }
+
+]
+);
+
+list.push(todo_list.firstQ);
+
+console.log(list)
+
+condition = todo_list.secondQ;
+
+}
+
+
+
+
+
